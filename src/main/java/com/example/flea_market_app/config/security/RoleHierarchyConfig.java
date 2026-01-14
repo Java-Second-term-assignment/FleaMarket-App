b0f20d971ba419efb2c7f03469136027238dc0a3
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-01-14 14:32:22 +0900

    RoleHierarchyConfigの設計

--- src/main/java/com/example/flea_market_app/config/security/RoleHierarchyConfig.java
+++ src/main/java/com/example/flea_market_app/config/security/RoleHierarchyConfig.java
@@ -1,8 +1,18 @@
 package com.example.flea_market_app.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 
 @Configuration
 public class RoleHierarchyConfig {
 
+	@Bean
+	RoleHierarchy roleHierarchy() {
+		return RoleHierarchyImpl.fromHierarchy("""
+				ROLE_ADMIN > ROLE_USER
+				ROLE_USER > ROLE_GUEST
+				""");
+	}
 }
