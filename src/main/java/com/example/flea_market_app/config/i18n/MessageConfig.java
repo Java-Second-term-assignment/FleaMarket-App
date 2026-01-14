d2c7e86fd7446fb3a3c946cfabd0d5706f7d14bd
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-01-14 15:46:20 +0900

    i18nのconfig設計

--- src/main/java/com/example/flea_market_app/config/i18n/MessageConfig.java
+++ src/main/java/com/example/flea_market_app/config/i18n/MessageConfig.java
@@ -1,5 +1,22 @@
 package com.example.flea_market_app.config.i18n;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
 public class MessageConfig {
+
+	@Bean
+	public MessageSource messageSource() {
+
+		ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+
+		source.setBasename("classpath:i18n/messages");
+		source.setDefaultEncoding("UTF-8");
+
+		return source;
+	}
 
 }
