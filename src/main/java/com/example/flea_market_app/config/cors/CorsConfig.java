0e474f85f50cee8eb40b867ca1e6fd765a5efb4d
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-01-14 15:57:23 +0900

    corsのconfig設計

--- src/main/java/com/example/flea_market_app/config/cors/CorsConfig.java
+++ src/main/java/com/example/flea_market_app/config/cors/CorsConfig.java
@@ -1,5 +1,36 @@
 package com.example.flea_market_app.config.cors;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
 public class CorsConfig {
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowedOrigins(List.of(
+				"http://localhost:3000",
+				"http://examole.com"
+
+		));
+
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		source.registerCorsConfiguration("api/**", config);
+
+		return source;
+	}
 
 }
