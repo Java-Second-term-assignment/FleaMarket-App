ee43731c45903962d4108321731e47f65f2fdcd7
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-01-14 15:41:02 +0900

    AWSの依存関係追加/awsのconfig設計

--- src/main/java/com/example/flea_market_app/config/aws/AwsClientConfig.java
+++ src/main/java/com/example/flea_market_app/config/aws/AwsClientConfig.java
@@ -1,5 +1,21 @@
 package com.example.flea_market_app.config.aws;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+
+@Configuration
 public class AwsClientConfig {
+
+	@Bean
+	public RekognitionClient rekognitionClient() {
+		return RekognitionClient.builder()
+
+				.region(Region.AP_NORTHEAST_1) // 現在は東京リージョンだが、修正必須(おそらく北バージニア)
+				.build();
+
+	}
 
 }
