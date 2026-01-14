061a9c21598dc34d53e3af237eb2c4dda8d0464c
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-01-14 16:51:43 +0900

    feat::config設計基本作業(JacksoConfgiは保留n)

--- src/main/java/com/example/flea_market_app/config/jackson/JacksonConfig.java
+++ src/main/java/com/example/flea_market_app/config/jackson/JacksonConfig.java
@@ -1,5 +1,19 @@
 package com.example.flea_market_app.config.jackson;
+/*
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import tools.jackson.databind.PropertyNamingStrategies;
+
+@Configuration
 public class JacksonConfig {
 
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+		return builder -> builder.propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+	}
 }
+*/
+
+// jacksonconfigに関しては、エラーが多いので、後日依存関係やversionも踏まえながら設計します。
