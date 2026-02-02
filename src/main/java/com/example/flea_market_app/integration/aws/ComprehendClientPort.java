27548b04b06c921936b369c170f73ba509344165
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2026-02-02 11:40:05 +0900

    feat::awsAdapter/エラーまわりの実装/修正

--- src/main/java/com/example/flea_market_app/integration/aws/ComprehendClientPort.java
+++ src/main/java/com/example/flea_market_app/integration/aws/ComprehendClientPort.java
@@ -1,5 +1,7 @@
 package com.example.flea_market_app.integration.aws;
 
-public interface ComprehendClientPort {
+import com.example.flea_market_app.integration.aws.dto.ModerationResult;
 
+public interface ComprehendClientPort {
+	ModerationResult analyzeText(String text);
 }
--- src/main/java/com/example/flea_market_app/integration/aws/FraudDetectorClientPort.java
+++ src/main/java/com/example/flea_market_app/integration/aws/FraudDetectorClientPort.java
@@ -1,5 +1,8 @@
 package com.example.flea_market_app.integration.aws;
 
-public interface FraudDetectorClientPort {
+import com.example.flea_market_app.integration.aws.dto.FraudScore;
+import com.example.flea_market_app.integration.aws.dto.FraudSignal;
 
+public interface FraudDetectorClientPort {
+	FraudScore evaluate(FraudSignal signal);
 }
